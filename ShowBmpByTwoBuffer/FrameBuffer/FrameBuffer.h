abed34228bec26ee4dc7334a7d34276a9ce7b273
ZhengPengqiao <157510312@qq.com>
2017-10-03 02:19:29 +0800

    1:写成FrameBuffer类来使用

---
+++ ShowBmpByTwoBuffer/FrameBuffer/FrameBuffer.h
@@ -0,0 +1,52 @@
+#ifndef _FRAMEBUFFER_H__
+#define _FRAMEBUFFER_H__
+#include <linux/fb.h>  
+
+typedef struct FrameBufferInfo
+{
+    
+}FrameBufferInfo;
+
+class FrameBuffer
+{
+public:
+	/* 构造函数 */
+	FrameBuffer();
+	/* 析构函数 */
+	~FrameBuffer();
+	/* 函数介绍 : 使用指定颜色刷新FrameBuffer*/
+	int ClearColor(int x, int y, int w, int h, int r, int g, int b);
+	/* 函数介绍 : 将指定的图像显示在指定的位置*/
+	int ShowBmp( int x, int y, char *bmpBuff, int bw, int bh);
+	/* 函数介绍 : 将指定的图像显示在指定的位置 */
+	int BufferShowBmp( char* fbBuffer, int fbw, int fbh, int x, int y, char *bmpBuff, int bw, int bh);
+	/* 函数介绍 : 使用指定颜色刷新FrameBuffer */
+	int BufferClearColor(char* fbBuffer, int fbw, int fbh, int x, int y, int w, int h, int r, int g, int b);
+	/* 函数介绍 : 初始化FrameBuffer */
+	int Init(char * fileName);
+	/* 函数介绍 : 交换绘制图像的buffer */
+	int swapDisplayBuffer();
+	/* 函数介绍 : 释放分配的资源*/
+	int Release();
+
+	struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
+    int bufferCount;
+    int nowDrawBuffer;		//供给FrameBuffer显示的缓冲区
+    int nowBackBuffer;		//后台绘制的缓冲区
+	char *pSwapBuffer;   	//为了实现双缓冲所分配的内存
+	char *pnowDrawBuffer;  	//供给FrameBuffer显示的缓冲区
+	char *pnowBackBuffer;  	//后台绘制的缓冲区
+	int width;				//FrameBuffer的宽
+	int height;				//FrameBuffer的高
+	int frameRateTime;      //刷新间隔us
+
+private:
+	/* 显示线程函数 */
+	static void * displayFun(void *arg);
+	char *fbp; 				//FrameBuffer的缓冲地址
+    int fd;  	
+    
+};
+
+#endif //_FRAMEBUFFER_H__
