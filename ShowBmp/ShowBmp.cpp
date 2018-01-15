81544e2134cddd848ff7166cd93e776b18a0235c
ZhengPengqiao <157510312@qq.com>
2018-01-15 15:06:48 +0800

    1:支持16bps,32bps

--- ShowBmp/ShowBmp.cpp
+++ ShowBmp/ShowBmp.cpp
@@ -25,15 +25,26 @@ typedef struct FrameBufferInfo
 /*
  * 函数名称 : ClearFrameBuff
  * 函数介绍 : 使用指定颜色刷新FrameBuffer
- * 参数介绍 : fbInfo:FrameBuffer相关信息， x,y,w,h：清空的矩形， r,g,b:使用的颜色
+ * 参数介绍 : fbInfo:FrameBuffer相关信息， x,y,w,h：清空的矩形， r,g,b:使用的颜色,
+ *           bps:framebuffer的bps
  * 返回值   : -1:失败，  0：成功
  */
-int ClearFrameBuff(FrameBufferInfo fbInfo, int x, int y, int w, int h, int r, int g, int b)
+int ClearFrameBuff(FrameBufferInfo fbInfo, int x, int y, int w, int h, int r, int g, int b, int bps)
 {
+    unsigned int rgb = 0;
     int drawW = 0;
     int drawH = 0;
-    int fbw = fbInfo.finfo.line_length/4;
+    int fbw = 0;
     int fbh = fbInfo.vinfo.yres;
+    switch( bps )
+    {
+        case 32:
+            fbw = fbInfo.finfo.line_length/4;
+        break;
+        case 16:
+            fbw = fbInfo.finfo.line_length/2;
+        break;
+    }
     if ( x + w > fbw )
     {
         drawW = fbw-x;
@@ -52,14 +63,35 @@ int ClearFrameBuff(FrameBufferInfo fbInfo, int x, int y, int w, int h, int r, in
         drawH = h;
     }
 
+    switch( bps )
+    {
+        case 32:
+            rgb = (r<<24) | (g<<16) | (b<<8);
+        break;
+        case 16:
+            rgb = (((unsigned(r) << 8) & 0xF800) | 
+                    ((unsigned(g) << 3) & 0x7E0) | 
+                    ((unsigned(b) >> 3)));
+        break;
+    }
+
     for( int i = 0; i < drawH; i++ )
     {
         for( int j = 0; j < drawW; j++ )
         {
-            fbInfo.fbp[fbw*4*(i+y)+(j+x)*4] = b;
-            fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+1] = g;
-            fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+2] = r;
-            fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+3] = 0;
+            switch( bps )
+            {
+                case 32:
+                    fbInfo.fbp[fbw*4*(i+y)+(j+x)*4] = (rgb>>24)&0xFF;
+                    fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+1] = (rgb>>16)&0xFF;
+                    fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+2] = (rgb>>8)&0xFF;
+                    fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+3] = (rgb)&0xFF;
+                break;
+                case 16:
+                    fbInfo.fbp[fbw*2*(i+y)+(j+x)*2] = (rgb>>8)&0xFF;
+                    fbInfo.fbp[fbw*2*(i+y)+(j+x)*2+1] = (rgb)&0xFF;
+                break;
+            }
         }
     }
     return 0;
@@ -69,15 +101,28 @@ int ClearFrameBuff(FrameBufferInfo fbInfo, int x, int y, int w, int h, int r, in
 /*
  * 函数名称 : ShowBmp
  * 函数介绍 : 将指定的图像显示在指定的位置
- * 参数介绍 : fbInfo:FrameBuffer信息， x,y:显示的位置， bmpBuff:图片数据， bw,bh:图片宽高
- * 返回值   : -1:失败，  0：成功, 
+ * 参数介绍 : fbInfo:FrameBuffer信息， x,y:显示的位置， bmpBuff:图片数据， bw,bh:图片宽高,  
+ *           bps:framebuffer的bps
+ * 返回值   : -1:失败，  0：成功,
  */
-int ShowBmp( FrameBufferInfo fbInfo, int x, int y, char *bmpBuff, int bw, int bh)
+int ShowBmp( FrameBufferInfo fbInfo, int x, int y, char *bmpBuff, int bw, int bh, int bps)
 {
+    unsigned int rgb = 0;
     int drawW = 0;
     int drawH = 0;
-    int fbw = fbInfo.finfo.line_length/4;
+    int fbw = 0;
     int fbh = fbInfo.vinfo.yres;
+
+    switch( bps )
+    {
+        case 32:
+            fbw = fbInfo.finfo.line_length/4;
+        break;
+        case 16:
+            fbw = fbInfo.finfo.line_length/2;
+        break;
+    }
+
     if ( x + bw > fbw )
     {
         drawW = fbw-x;
@@ -100,10 +145,23 @@ int ShowBmp( FrameBufferInfo fbInfo, int x, int y, char *bmpBuff, int bw, int bh
     {
         for( int j = 0; j < drawW; j++ )
         {
-            fbInfo.fbp[fbw*4*(i+y)+(j+x)*4] = bmpBuff[i*bw*3+j*3+0];
-            fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+1] = bmpBuff[i*bw*3+j*3+1];
-            fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+2] = bmpBuff[i*bw*3+j*3+2];
-            fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+3] = 255;
+                switch( bps )
+                {
+                    case 32:
+                        fbInfo.fbp[fbw*4*(i+y)+(j+x)*4] = bmpBuff[i*bw*3+j*3+0];
+                        fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+1] = bmpBuff[i*bw*3+j*3+1];
+                        fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+2] = bmpBuff[i*bw*3+j*3+2];
+                        fbInfo.fbp[fbw*4*(i+y)+(j+x)*4+3] = 255;
+                    break;
+                    case 16:
+                        rgb = (((bmpBuff[i*bw*3+j*3+2] << 8) & 0xF800) | 
+                                ((bmpBuff[i*bw*3+j*3+1] << 3) & 0x7E0) | 
+                                ((bmpBuff[i*bw*3+j*3+0] >> 3)));
+                        fbInfo.fbp[fbw*2*(i+y)+(j+x)*2] = (rgb)&0xFF;
+                        fbInfo.fbp[fbw*2*(i+y)+(j+x)*2+1] = (rgb>>8)&0xFF;
+                    break;
+                }
+
         }
     } 
 }
@@ -115,7 +173,20 @@ int main ( int argc, char *argv[] )
     int topx = 0;
     int topy = 0;
     int frameCount = 0;
+    int bps = 0;
     struct timeval ts, te;
+
+    int r = 0;
+    int g = 0;
+    int b = 0;
+
+    if(argc == 4)
+    {
+        r = atoi(argv[1]);
+        g = atoi(argv[2]);
+        b = atoi(argv[3]);
+        printf("clear r=%d, g=%d, b=%d \n", r, g, b);
+    }
 
     /*打开设备文件*/  
     fbInfo.fd = open(DEV_NAME, O_RDWR);  
@@ -167,6 +238,7 @@ int main ( int argc, char *argv[] )
     printf("vinfo.blue   offset=%d   length=%d   msb_right=%d\n",  fbInfo.vinfo.blue.offset,  fbInfo.vinfo.blue.length,  fbInfo.vinfo.blue.msb_right);
     printf("vinfo.transp   offset=%d   length=%d   msb_right=%d\n",  fbInfo.vinfo.transp.offset,  fbInfo.vinfo.transp.length,  fbInfo.vinfo.transp.msb_right);
     printf("vinfo.bits_per_pixel=%d\n", fbInfo.vinfo.bits_per_pixel);
+    bps = fbInfo.vinfo.bits_per_pixel;
     printf("vinfo.xres_virtual=%d vinfo.yres_virtual=%d\n", fbInfo.vinfo.xres_virtual, fbInfo.vinfo.yres_virtual);
     printf("vinfo.xoffset=%d  vinfo.yoffset=%d\n", fbInfo.vinfo.xoffset, fbInfo.vinfo.yoffset);
     printf("vinfo.xresXvinfo.yres=%dX%d\n", fbInfo.vinfo.xres, fbInfo.vinfo.yres);  
@@ -221,9 +293,9 @@ int main ( int argc, char *argv[] )
     printf("fileName=%s  width=%d height=%d length=%d\n", bmpInfo->fileName, bmpInfo->imagewidth, bmpInfo->imageheight, bmpInfo->pixellength);
     while(1)
     {
-        ClearFrameBuff(fbInfo, 0, 0, fbInfo.vinfo.xres, fbInfo.vinfo.yres, 0, 0, 0);
+        ClearFrameBuff(fbInfo, 0, 0, fbInfo.vinfo.xres, fbInfo.vinfo.yres, r, g, b, bps);
         ShowBmp(fbInfo, topx, topy,\
-            bmpInfo->pixeldata, bmpInfo->imagewidth, bmpInfo->imageheight);
+            bmpInfo->pixeldata, bmpInfo->imagewidth, bmpInfo->imageheight, bps);
         usleep(33000);
 
         topx+=3;
